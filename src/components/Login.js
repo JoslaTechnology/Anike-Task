84a802f806b54cefebd93b9e3fed5fa3df37fb54
Adewunmi <adewunmi_akinsanya@yahoo.com>
2018-06-18 13:28:21 +0100

    updates

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+ 
+export default class Login extends Component { 
+  render() {
+    return (
+        <div>
+          
+        </div>
+    )
+  }
+}
